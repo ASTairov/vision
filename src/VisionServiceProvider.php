940e4b38b174b1c48addf867c736298733076b8c
ASTairov <43048778+ASTairov@users.noreply.github.com>
2020-05-02 00:18:41 +0300

    service provider

--- src/VisionServiceProvider.php
+++ src/VisionServiceProvider.php
@@ -1,28 +1,67 @@
 <?php
 
-namespace App\Providers;
+namespace ASTairov\Seat\Vision;
 
 use Illuminate\Support\ServiceProvider;
 
 class VisionServiceProvider extends ServiceProvider
 {
     /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap services.
+     * Bootstrap the application services.
      *
      * @return void
      */
     public function boot()
     {
-        //
+        //$this->addCommands();
+        $this->add_routes();
+        // $this->add_middleware($router);
+        $this->add_views();
+        $this->add_publications();
+        //$this->add_translations();
+    }
+
+    /**
+     * Include the routes.
+     */
+    public function add_routes()
+    {
+        if (! $this->app->routesAreCached()) {
+            include __DIR__ . '/Http/routes.php';
+        }
+    }
+
+    //public function add_translations()
+    //{
+    //    $this->loadTranslationsFrom(__DIR__ . '/lang', 'fitting');
+    //}
+
+    /**
+     * Set the path and namespace for the views.
+     */
+    public function add_views()
+    {
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'vision');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/Config/vision.config.php', 'vision.config');
+
+        $this->mergeConfigFrom(
+            __DIR__ . '/Config/vision.sidebar.php', 'package.sidebar');
+
+        $this->mergeConfigFrom(
+            __DIR__ . '/Config/vision.permissions.php', 'web.permissions');
+    }
+
+    private function addCommands()
+    {
     }
 }
