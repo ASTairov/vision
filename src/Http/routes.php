781acc54401d972bb7dd433b499e924c903b37f9
ASTairov <43048778+ASTairov@users.noreply.github.com>
2020-05-02 03:37:42 +0300

    middleware

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,6 +1,7 @@
 <?php
 Route::group([
     'namespace' => 'ASTairov\Seat\Vision\Http\Controllers',
+    'middleware' => ['web','auth'],
     'prefix' => 'vision'
 ], function () {
     Route::get('/vision', function () {
