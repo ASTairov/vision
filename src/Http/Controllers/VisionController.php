4ae9bdf46eeb3a654ad8afd9a3d415afb1fd4f5b
ASTairov <43048778+ASTairov@users.noreply.github.com>
2020-05-03 00:44:08 +0300

    test

--- src/Http/Controllers/VisionController.php
+++ src/Http/Controllers/VisionController.php
@@ -1,4 +1,4 @@
-<?
+<?php
 namespace ASTairov\Seat\Vision\Http\Controllers;
 
 use Seat\Services\Repositories\Character\Info;
@@ -7,16 +7,26 @@ use Seat\Services\Repositories\Configuration\UserRespository;
 use Seat\Web\Http\Controllers\Controller;
 use Seat\Web\Models\Acl\Role;
 
-use Seat\Eveapi\Models\Location\CharacterLocation;
 use Seat\Eveapi\Models\Location\CharacterOnline;
 use Seat\Eveapi\Models\Location\CharacterShip;
+
+use Seat\Eveapi\Models\Location\CharacterLocation;
 use Seat\Eveapi\Jobs\Location\Character;
+
+use Seat\Eveapi\Models\Character\CharacterInfo;
+use Seat\Eveapi\Models\Sde\MapDenormalize;
+use Seat\Eveapi\Models\Sde\StaStation;
+use Seat\Eveapi\Models\Universe\UniverseName;
+use Seat\Eveapi\Models\Universe\UniverseStructure;
+use Seat\Services\Repositories\Corporation\Members;
+use Yajra\DataTables\DataTables;
+
 
 class VisionController extends Controller
 {
     public function getVisionView()
     {
-        $visiongroups= Location::handle('95021222');
+        $visiongroups= Location::handle();
         return view('vision::vision',compact('visiongroups'));
     }
 }
