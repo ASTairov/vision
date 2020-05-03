9c166599fb177d827e0e76d6cb7f8506013a6589
ASTairov <43048778+ASTairov@users.noreply.github.com>
2020-05-03 17:22:14 +0300

    ы

--- src/Http/Controllers/VisionController.php
+++ src/Http/Controllers/VisionController.php
@@ -1,6 +1,16 @@
 <?php
 namespace ASTairov\Seat\Vision\Http\Controllers;
 
+use Seat\Services\Repositories\Character\Info;
+use Seat\Services\Repositories\Character\Skills;
+use Seat\Services\Repositories\Configuration\UserRespository;
+use Seat\Web\Http\Controllers\Controller;
+use Seat\Web\Models\Acl\Role;
+use Seat\Eveapi\Models\Alliances\Alliance;
+use Seat\Eveapi\Models\Alliances\AllianceMember;
+use Seat\Eveapi\Models\Character\CharacterInfo;
+use Seat\Eveapi\Models\Corporation\CorporationInfo;
+
 use Seat\Eveapi\Models\Location\CharacterLocation;
 use Seat\Eveapi\Models\Killmails;
 use Seat\Eveapi\Jobs\Location\Character;
