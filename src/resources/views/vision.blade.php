02da29f3b5d9532ee700ac15722b8310873a1825
ASTairov <43048778+ASTairov@users.noreply.github.com>
2020-05-07 21:54:19 +0300

    awd

--- src/resources/views/vision.blade.php
+++ src/resources/views/vision.blade.php
@@ -5,24 +5,12 @@
 @extends('web::layouts.grids.12')
 
 @section('content')
-    <div class="tab-content">
-        <div class="tab-pane " id="auto_group">
-            <div class="row">
-                <div class="col-md-12">
-                    <h3>h3</h3>
-                    <p>p1</p>
-                </div>
-            </div>
-            <div class="row">
-                    <div class="alert alert-info">
-                        <h3>
-                             {{ $char_name }}
-                        </h3>
-                        <p>
-                            {{ $char_loc }}
-                        </p>
-                    </div>
-            </div>
-        </div>
-    </div>
+
+{{ $char_name }}
+
+{{ $char_loc->solar_system_id }}
+
 @endsection
+@push('javascript')
+  @include('web::includes.javascript.id-to-name')
+@endpush
